1568a6684b7b5b583e3dacbe80af106c3c57bdf4
Njuci <augustinnjuci@gmail.com>
2024-07-17 21:11:58 +0200

    adminsite register

--- horaire_univ/admin.py
+++ horaire_univ/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Domaine,Faculte,Filiere,Mention,Promotion,Unite_Ens,Anacad,Elenent_Const,Partie_ec,Programme_ec,Disponibilite,Horaire
 # Register your models here.
+admin
+admin.site.register(Domaine)
+admin.site.register(Faculte)
+admin.site.register(Filiere)
+admin.site.register(Mention)
+admin.site.register(Promotion)
+admin.site.register(Unite_Ens)
+admin.site.register(Anacad)
+admin.site.register(Elenent_Const)
+admin.site.register(Partie_ec)
+admin.site.register(Programme_ec)
+admin.site.register(Disponibilite)
+#admin.site.register(MyUser)
