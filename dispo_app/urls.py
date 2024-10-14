b50c992e93042f90a6dd98718e45e592f3dfda00
Njuci <augustinnjuci@gmail.com>
2024-10-14 16:43:46 +0200

    'get horaire enseignant'

--- dispo_app/urls.py
+++ dispo_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import DisponibiliteView,Get_date_cours,GetHorairebyPromo
+from .views import DisponibiliteView,Get_date_cours,GetHorairebyPromo,GetHorairebyEnseignant
 
 
 urlpatterns = [
     path('disponibilite/', DisponibiliteView.as_view()),
     #path('horaire/', HoraireView.as_view()),
     path('get_disponibilite/<str:id_partie_ec>', Get_date_cours.as_view()),
-    path('get_horaire_by_promo/<str:id_promotion>/<str:id_anacad>', GetHorairebyPromo.as_view())
+    path('get_horaire_by_promo/<str:id_promotion>/<str:id_anacad>', GetHorairebyPromo.as_view()),
+    path('get_horaire_by_enseignant/<str:id_enseignant>/<str:id_anacad>', GetHorairebyEnseignant.as_view())
     #path('semestre/', SemestreView.as_view())
 ]
 
